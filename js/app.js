55cdf706700e7d5d490e54375aa6414f1d0e9bf5
jgoode7 <78943773+jgoode7@users.noreply.github.com>
2021-04-26 10:24:59 -0700

    Pre-peer Review
    last updates, including JS search bar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-baguetteBox.run('.gallery');​
+window.addEventListener('load', function() {
+  baguetteBox.run('.gallery');
+});
+
+const img = document.getElementsByTagName('a');
+const input = document.getElementById('search-bar')
+
+input.addEventListener('keyup', search => {
+    const searchInput = search.target.value.toLowerCase();
+    for( i = 0; i < img.length; i++) {
+        const inputValue = img[i].getAttribute('data-caption');
+        if (inputValue.toLocaleLowerCase().indexOf(searchInput) > -1) {
+            img[i].style.display = "";
+        } else {
+            img[i].style.display= "none";
+        }
+    }
+});
